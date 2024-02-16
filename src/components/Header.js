66342530c9f55ca1aa445a0cc4b1baa07ce2bb66
胡家萁 <hujiaqi@hujiaqideMacBook-Pro.local>
2024-02-16 23:34:17 +1100

    add welcome header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header_container'>
+      <p className='Header_welcome'>WELCOME TO MY PROFILE</p>
+    </div>
+  )
+}
+
+export default Header
