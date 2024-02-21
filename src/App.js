b190b44fbc52db5278a641abbdcaf50d23dc458a
胡家萁 <hujiaqi@hujiaqideMacBook-Pro.local>
2024-02-21 16:02:06 +1100

    update-about-me

--- src/App.js
+++ src/App.js
@@ -7,29 +7,6 @@ function App() {
     <div className='App'>
       <Cover />
       <AboutMe />
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
-      <p>4</p>
-      <p>5</p>
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
-      <p>4</p>
-      <p>5</p>
-      <p>a</p>
-      <p>b</p>
-      <p>c</p>
-      <p>d</p>
-      <p>e</p>
-      <p>f</p>
-      <p>g</p>
-      <p>h</p>
-      <p>i</p>
-      <p>j</p>
-      <p>k</p>
-      <p>l</p>
-      
     </div>
   );
 }
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import './AboutMe.css';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 const About_me = () => {
   return (
-    <div className='about-container'>
-      
+    <div className="about-container">
+      <h2 className="about-me-text">ABOUT ME</h2>
+      <div className="about-info-section">
+        <div className="about-info-selfimg-container">
+          <Image src="/images/id_photo.png" roundedCircle style={{ maxWidth: '400px', maxHeight: '400px' }} />
+        </div>
+        <div className="about-info-text-container">
+          <Container>
+            <Row>
+              <h2>What I hope to do</h2>
+              <p>My mission as a developer is to contribute to applications that empower and improve lives.
+                Yes, let's change the world together!</p>
+            </Row>
+            <Row>
+              <h2>What I am doing</h2>
+              <p>I recently shared all of the resources and strategies I used to land many software engineer interviews.</p>
+            </Row>
+            <Row>
+              <h2>Skills</h2>
+            </Row>
+            <Row>
+              <Col><h5>Languages</h5></Col>
+              <Col><h5>Frames</h5></Col>
+              <Col><h5>Tools</h5></Col>
+            </Row>
+            <Row>
+              <Col>
+              <p>Java, Python, JavaScript</p>
+              </Col>
+              <Col>React, Node.js</Col>
+              <Col>Git</Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
     </div>
   )
 }
